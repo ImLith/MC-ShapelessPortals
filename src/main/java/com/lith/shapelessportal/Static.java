220202116c490cbe66071898037bfff0f0983264
ImLith <baranauskas.edvinas26@gmail.com>
2024-04-02 21:01:11 +0300

    init

---
+++ src/main/java/com/lith/shapelessportal/Static.java
@@ -0,0 +1,8 @@
+package com.lith.shapelessportal;
+
+import java.util.logging.Logger;
+
+public class Static {
+    public static final String pluginName = "ShapelessPortal";
+    public static final Logger log = Logger.getLogger(Static.pluginName);
+}
